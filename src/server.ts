1d2cc28a8862ae2998c2de5a3152cb2e62c7a155
zchelalo <eduardosaavedra687@gmail.com>
2024-08-17 01:59:53 -0700

    update: remove i18next

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import { responseMiddleware } from './middlewares/response'
-import { changeLanguage } from './middlewares/lang'
 
 import { router as userRouter } from './app/user/infrastructure/router'
 
@@ -12,7 +11,6 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(responseMiddleware)
-app.use(changeLanguage)
 
 const whitelist = ['http://localhost:5173']
 app.use(cors({
--- src/utils/translation.ts
+++
@@ -1,5 +0,0 @@
-import { i18n } from 'src/helpers/i18n/config'
-
-export const translateText = (key: string): string => {
-  return i18n.t(key)
-}
