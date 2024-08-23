9295c5c2dd2a6080dfbf98f05d91dba956eb6231
zchelalo <eduardosaavedra687@gmail.com>
2024-08-22 17:19:25 -0700

    update: remove unnecessary env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -16,17 +16,7 @@ const envVars = {
   DB_PORT: Number(process.env.DB_PORT),
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
-  DB_NAME: process.env.DB_NAME,
-
-  EMAIL_SERVER: process.env.EMAIL_SERVER,
-  EMAIL_PORT: Number(process.env.EMAIL_PORT),
-  EMAIL_SECURE: process.env.EMAIL_SECURE === true,
-  EMAIL_USER: process.env.EMAIL_USER,
-  EMAIL_PASS: process.env.EMAIL_PASS,
-
-  ACCESS_TOKEN_EXPIRATION_MINUTES: Number(process.env.ACCESS_TOKEN_EXPIRATION_MINUTES),
-  REFRESH_TOKEN_EXPIRATION_DAYS: Number(process.env.REFRESH_TOKEN_EXPIRATION_DAYS),
-  RECOVERY_TOKEN_EXPIRATION_MINUTES: Number(process.env.RECOVERY_TOKEN_EXPIRATION_MINUTES)
+  DB_NAME: process.env.DB_NAME
 }
 
 /**
@@ -42,17 +32,7 @@ const config = z.object({
   DB_PORT: z.number().int(),
   DB_USER: z.string(),
   DB_PASS: z.string(),
-  DB_NAME: z.string(),
-
-  EMAIL_SERVER: z.string(),
-  EMAIL_PORT: z.number().int(),
-  EMAIL_SECURE: z.boolean(),
-  EMAIL_USER: z.string(),
-  EMAIL_PASS: z.string(),
-
-  ACCESS_TOKEN_EXPIRATION_MINUTES: z.number().int(),
-  REFRESH_TOKEN_EXPIRATION_DAYS: z.number().int(),
-  RECOVERY_TOKEN_EXPIRATION_MINUTES: z.number().int()
+  DB_NAME: z.string()
 })
 
 /**
