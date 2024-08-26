cd51df3825c072aa2480b55ee9c12bf7ca944e4a
zchelalo <eduardosaavedra687@gmail.com>
2024-08-25 21:06:56 -0700

    add: routes to sign up and sign out

--- src/modules/auth/infrastructure/router.ts
+++ src/modules/auth/infrastructure/router.ts
@@ -5,8 +5,9 @@ import { PostgresRepository as AuthPostgresRepository } from './repositories/pos
 import { AuthUseCase } from '../application/use_cases/auth'
 import { AuthController } from './controller'
 
+import { authMiddleware } from 'src/middlewares/auth'
 import { validateData, Type } from 'src/middlewares/validator'
-import { signInSchema } from '../application/schemas/auth'
+import { signInSchema, signUpSchema } from '../application/schemas/auth'
 
 const router = Router()
 
@@ -16,5 +17,7 @@ const useCase = new AuthUseCase(authRepository, userRepository)
 const authController = new AuthController(useCase)
 
 router.post('/auth/sign-in', validateData(signInSchema, Type.BODY), authController.signIn)
+router.post('/auth/sign-up', validateData(signUpSchema, Type.BODY), authController.signUp)
+router.post('/auth/sign-out', authMiddleware, authController.signOut)
 
 export { router }
