eae636563490a494221d77d21792653823aaefc4
zchelalo <eduardosaavedra687@gmail.com>
2024-08-25 18:20:00 -0700

    add: authMiddleware to user routes

--- src/modules/user/infrastructure/router.ts
+++ src/modules/user/infrastructure/router.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express'
+
 import { PostgresRepository } from './repositories/postgres'
 import { MemoryRepository } from './repositories/memory'
 import { UserUseCase } from '../application/use_cases/user'
 import { UserController } from './controller'
+
 import { paginationMiddleware } from 'src/middlewares/pagination'
+import { authMiddleware } from 'src/middlewares/auth'
 import { validateData, Type } from 'src/middlewares/validator'
 import { createUserSchema, getUserByIDSchema } from '../application/schemas/user'
 
@@ -13,8 +16,8 @@ const repository = process.env.NODE_ENV === 'test' ? new MemoryRepository() : ne
 const useCase = new UserUseCase(repository)
 const userController = new UserController(useCase)
 
-router.get('/users', paginationMiddleware, userController.getUsers)
-router.get('/users/:id', validateData(getUserByIDSchema, Type.PARAMS), userController.getUserById)
-router.post('/users', validateData(createUserSchema, Type.BODY), userController.createUser)
+router.get('/users', authMiddleware, paginationMiddleware, userController.getUsers)
+router.get('/users/:id', authMiddleware, validateData(getUserByIDSchema, Type.PARAMS), userController.getUserById)
+router.post('/users', authMiddleware, validateData(createUserSchema, Type.BODY), userController.createUser)
 
 export { router }
