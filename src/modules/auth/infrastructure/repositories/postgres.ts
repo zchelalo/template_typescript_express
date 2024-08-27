39d7f789d62fdb21bcdac948bafcfa7ce750493e
zchelalo <eduardosaavedra687@gmail.com>
2024-08-26 22:00:22 -0700

    add: docs to auth postgres repository

--- src/modules/auth/infrastructure/repositories/postgres.ts
+++ src/modules/auth/infrastructure/repositories/postgres.ts
@@ -13,8 +13,26 @@ import { NotFoundError } from 'src/helpers/errors/custom_error'
  * This class implements the AuthRepository interface to provide methods for interacting with the PostgreSQL database.
  * 
  * @implements {AuthRepository}
+ * @example
+ * ```ts
+ * const repository = new PostgresRepository()
+ * ```
 */
 export class PostgresRepository implements AuthRepository {
+  /**
+   * Get a token by user ID and token value.
+   * 
+   * @param {string} id - The user ID.
+   * @param {string} tokenValue - The token value.
+   * @returns {Promise<TokenValue>} A promise that resolves with the token value.
+   * @throws {NotFoundError} If the token is not found.
+   * @example
+   * ```ts
+   * const userId = '938d6f5b-b4a6-4669-a514-ddb3a23621fc'
+   * const tokenValue = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3ZDgwNzQwNi1hNTQzLTRlMWYtYjAxOS1jOGIwNWQ1OGM1OWIiLCJpYXQiOjE3MjQ2MzExMjksImV4cCI6MTcyNTkyNzEyOX0.l7WXdoTopPRqeK-TNIgJtCoR863Yot5cJC-jV3v6DwJtrvH9wjqGFPHpgo00z4d9jCbMTEBnUfv2NkFCk4ecPt4YTledruAuxQoULk3NqoaXhn4wlKhQj7w14ngldir_pud4SxXJnfaw_zd1xg6Gd7rDH-LAWUYaNyvs8qt2CRra7pnBA6tBUvrO58HYReJRQU-GQP9PWRmRC4G8H3tpnGEybn4NcNCn-rO-PIgABZ1I3Len1y8ibKMrz53Rc1PTUTInD96RORM5zp5c06qkyUjW9AThFQwmYP9Yzo4z3fBsuvqQFha31lWoqzP5LNk2iOHECuequuLPThtNWdsRyw'
+   * const token = await repository.getTokenByUserIdAndValue(userId, tokenValue)
+   * ```
+  */
   async getTokenByUserIdAndValue(id: string, tokenValue: string): Promise<TokenValue> {
     const tokenObtained = await db
       .select({
@@ -37,32 +55,41 @@ export class PostgresRepository implements AuthRepository {
     return tokenObtained[0]
   }
 
-  async saveToken(id: string, tokenValue: string, userId: string, tokenTypeId: string): Promise<void> {
+  /**
+   * Save a token.
+   * 
+   * @param {TokenValue} tokenValue - The token value.
+   * @returns {Promise<void>} A promise that resolves with a void.
+   * @example
+   * ```ts
+   * const tValue = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3ZDgwNzQwNi1hNTQzLTRlMWYtYjAxOS1jOGIwNWQ1OGM1OWIiLCJpYXQiOjE3MjQ2MzExMjksImV4cCI6MTcyNTkyNzEyOX0.l7WXdoTopPRqeK-TNIgJtCoR863Yot5cJC-jV3v6DwJtrvH9wjqGFPHpgo00z4d9jCbMTEBnUfv2NkFCk4ecPt4YTledruAuxQoULk3NqoaXhn4wlKhQj7w14ngldir_pud4SxXJnfaw_zd1xg6Gd7rDH-LAWUYaNyvs8qt2CRra7pnBA6tBUvrO58HYReJRQU-GQP9PWRmRC4G8H3tpnGEybn4NcNCn-rO-PIgABZ1I3Len1y8ibKMrz53Rc1PTUTInD96RORM5zp5c06qkyUjW9AThFQwmYP9Yzo4z3fBsuvqQFha31lWoqzP5LNk2iOHECuequuLPThtNWdsRyw'
+   * const userId = '938d6f5b-b4a6-4669-a514-ddb3a23621fc'
+   * const tokenTypeId = '938d6f5b-b4a6-4669-a514-ddb3a23621fc'
+   * const tokenValue = new TokenValue(tValue, userId, tokenTypeId)
+   * await repository.saveToken(tokenValue)
+   * ```
+  */
+  async saveToken(tokenValue: TokenValue): Promise<void> {
     await db
       .insert(token)
-      .values({
-        id,
-        token: tokenValue,
-        userId,
-        tokenTypeId
-      })
+      .values(tokenValue)
   }
 
+  /**
+   * Revoke a token by the user ID and the token value.
+   * 
+   * @param {string} id - The user ID.
+   * @param {string} tokenValue - The token value.
+   * @returns {Promise<void>} A promise that resolves with a void.
+   * @example
+   * ```ts
+   * const userId = '938d6f5b-b4a6-4669-a514-ddb3a23621fc'
+   * const tokenValue = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3ZDgwNzQwNi1hNTQzLTRlMWYtYjAxOS1jOGIwNWQ1OGM1OWIiLCJpYXQiOjE3MjQ2MzExMjksImV4cCI6MTcyNTkyNzEyOX0.l7WXdoTopPRqeK-TNIgJtCoR863Yot5cJC-jV3v6DwJtrvH9wjqGFPHpgo00z4d9jCbMTEBnUfv2NkFCk4ecPt4YTledruAuxQoULk3NqoaXhn4wlKhQj7w14ngldir_pud4SxXJnfaw_zd1xg6Gd7rDH-LAWUYaNyvs8qt2CRra7pnBA6tBUvrO58HYReJRQU-GQP9PWRmRC4G8H3tpnGEybn4NcNCn-rO-PIgABZ1I3Len1y8ibKMrz53Rc1PTUTInD96RORM5zp5c06qkyUjW9AThFQwmYP9Yzo4z3fBsuvqQFha31lWoqzP5LNk2iOHECuequuLPThtNWdsRyw'
+   * await repository.revokeTokenByUserIdAndValue(userId, tokenValue)
+   * ```
+  */
   async revokeTokenByUserIdAndValue(id: string, tokenValue: string): Promise<void> {
-    const tokenObtained = await db
-      .select({
-        tokenValue: token.token
-      })
-      .from(token)
-      .where(and(
-        eq(token.userId, id),
-        eq(token.token, tokenValue)
-      ))
-      .limit(1)
-
-    if (!tokenObtained.length) {
-      throw new NotFoundError('token')
-    }
+    await this.getTokenByUserIdAndValue(id, tokenValue)
 
     await db
       .delete(token)
@@ -71,6 +98,18 @@ export class PostgresRepository implements AuthRepository {
       ))
   }
 
+  /**
+   * Get a token type ID by the key.
+   * 
+   * @param {string} key - The key of the token type.
+   * @returns {Promise<TokenTypeValue>} A promise that resolves with a token type value.
+   * @throws {NotFoundError} If the token type is not found.
+   * @example
+   * ```ts
+   * const key = 'refresh'
+   * const tokenTypeId = await repository.getTokenTypeIdByKey(key)
+   * ```
+  */
   async getTokenTypeIdByKey(key: string): Promise<TokenTypeValue> {
     const tokenTypeObtained = await db
       .select({
